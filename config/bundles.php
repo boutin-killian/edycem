27fe2fe062bf75a4ee06cf0e63bae50f33930b3f
Killian BOUTIN <killian.boutin@gosselink.fr>
2019-07-11 11:14:21 +0200

    Deploy

--- config/bundles.php
+++ config/bundles.php
@@ -21,5 +21,4 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
